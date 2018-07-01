5b770c894c5b149b3565ecdbae6685835de80365
vijay.pratap@lumatagroup.com <vijay.pratap@lumatagroup.com>
2018-07-01 17:07:40 +0530

    updated variable name

--- src/main/java/com/tiaa/vijay/Worker.java
+++ src/main/java/com/tiaa/vijay/Worker.java
@@ -7,7 +7,6 @@ public class Worker extends Thread{
 	int X;
 	int Y ;
 	int sleepInSeconds;
-	//int totalProduct = 0;
 	private AtomicInteger totalProduct = new AtomicInteger();
 	long totalTimeTaken =0;
 	Object unfinishedgoodsLock = new Object();
@@ -19,16 +18,15 @@ public class Worker extends Thread{
 	}
 
 
-	public boolean getUnfinishedGood() {
-		
+	public boolean getUnfinishedGood() 
+	{
 		boolean IsRawMaterialavailable = false; 
-		
 		synchronized (unfinishedgoodsLock) 
 		{			
 				if(X ==0 || Y == 0 ){     // Checking if enough raw material is available or not 
-					IsRawMaterialavailable =  false;
+					IsRawMaterialavailable =  false;      ///False means raw material has been finished
+														  //True means raw material is remaining
 				}
-				
 				else {
 					for(int i=1;i<=X;i++){
 						if(i*2 <= Y){
@@ -40,7 +38,6 @@ public class Worker extends Thread{
 					}
 				}			
 		}
-		
 		return IsRawMaterialavailable;
 	}
 	
@@ -48,18 +45,18 @@ public class Worker extends Thread{
 	
 	public void assembleProduct() {
 		
-		boolean rawMaterialOK = getUnfinishedGood();		
+		boolean IsRawMaterialavailable = getUnfinishedGood();		
 		
-		if(rawMaterialOK){
-		do {		
-			try {								
-				Thread.sleep(sleepInSeconds*1000); 
-				totalProduct.incrementAndGet();
-			} catch (InterruptedException e) {
-				e.printStackTrace();
+			if(IsRawMaterialavailable){
+				do {		
+					try {								
+						Thread.sleep(sleepInSeconds*1000); 
+						totalProduct.incrementAndGet();
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				}while(getUnfinishedGood());
 			}
-		}while(getUnfinishedGood());
-		}
 	}
 	
 
